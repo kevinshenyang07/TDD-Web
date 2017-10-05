533ef0dade1c9974c35cf547599d5b8592923c01
kevinshenyang07 <kevinshenyang07@gmail.com>
2017-10-05 15:00:55 -0700

    Write tests for home_page view of lists app

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
+from lists.views import home_page
 
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')  # find the view function a URL is mapped to
+        self.assertEqual(found.func, home_page)
+    
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))

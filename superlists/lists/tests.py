f7236e49e9284c180c2cb58e8de5a12b8559d93d
kevinshenyang07 <kevinshenyang07@gmail.com>
2017-10-05 22:44:42 -0700

    User can add to-do item and make POST request.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -14,6 +14,11 @@ class HomePageTest(TestCase):
         found = resolve('/')  # find the view function a URL is mapped to
         self.assertEqual(found.func, home_page)
     
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_next': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
